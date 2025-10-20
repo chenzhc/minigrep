272a38c135b02cbb5e6cb2fe04254210b722e8b7
chenzc <80132938@qq.com>
2025-10-20 17:05:49 +0800

    add

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,13 @@ pub fn init() {
 pub fn run(config: Config) -> anyhow::Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
     
-    for line in search(&config.query, &contents) {
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
         info!("{}", line);
     }
 
@@ -29,6 +35,7 @@ pub fn run(config: Config) -> anyhow::Result<(), Box<dyn Error>> {
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -40,7 +47,9 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Ok(Config { query, filename })
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config { query, filename, case_sensitive })
     }
 }
 
@@ -54,4 +63,17 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
         }
     }
     return results;
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
